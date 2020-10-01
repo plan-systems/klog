5427f407c61c2731003880dbe8cd4c18c10554d0
Drew O'Meara <drew2019@plan-systems.org>
2020-09-30 23:35:51 -0500

    added terminal color test

---
+++ 256-color-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+ 
+for fgbg in 38 48 ; do # Foreground / Background
+    for color in {0..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %03s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color + 1) % 6)) == 4 ] ; then
+            echo # New line
+        fi
+    done
+    echo # New line
+done
+ 
+exit 0
